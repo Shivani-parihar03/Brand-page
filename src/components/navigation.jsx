3f9df5b6331994ac64e503e23ace8999df2f7dfd
Shivani-parihar03 <shivaniparihar678@gmail.com>
2024-03-19 20:49:22 +0530

    Brand-page

---
+++ src/components/navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = ()=>{
+
+    return   (<nav className="container">
+        <div className="logo">
+            <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+        <li href="#">Menu</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+        <li href="#">Location</li>
+       </ul>
+    
+       <button>button</button>
+       </nav>)  ;
+};
+
+export default Navigation
